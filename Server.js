c287f0038077d3fe357d65a3d255a48a7a6c1c68
Moumita Ray <moumitacloud08@gmail.com>
2021-08-24 12:15:07 +0530

    Authentication

--- Server.js
+++ Server.js
@@ -1,6 +1,26 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(9000);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  var authObj = req.body;
+  var response;
+  if(authObj.userName == 'Soumita' && authObj.password == 'abc'){
+    response = {'code':'000','message':'Login Successful','data':''}
+  }else{
+    response = {'code':'111','message':'Authenticaion Failed','data':''}
+  }  
+  res.send(response)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
